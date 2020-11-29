ea2b77caf5f2933dedb934bce551fcfc9da4d969
John Whitington <john@coherentgraphics.co.uk>
2020-11-29 17:06:50 +0000

    more

---
+++ Jcpdf.java
@@ -0,0 +1,10 @@
+/* Jcpdf.java */
+
+public class Jcpdf {
+    native void helloFromC(); /* (1) */
+    static public void main(String argv[]) {
+        System.loadLibrary("jcpdf"); /* (2) */
+        Jcpdf jcpdf = new Jcpdf();
+        jcpdf.helloFromC(); /* (3) */
+    }
+}
--- jcpdfwrapper.c
+++ jcpdfwrapper.c
@@ -1,9 +1,9 @@
-/* ctest.c */
+/* jcpdfwrapper.c */
 
 #include <jni.h>
 #include <stdio.h>
 
-JNIEXPORT void JNICALL Java_HelloWorld_helloFromC
+JNIEXPORT void JNICALL Java_Jcpdf_helloFromC
   (JNIEnv * env, jobject jobj)
 {
     printf("Hello from C!\n");
