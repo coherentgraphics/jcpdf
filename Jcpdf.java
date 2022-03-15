c5c4985f47eb4b94bd6d542d4094da50c133f2db
John Whitington <john@coherentgraphics.co.uk>
2022-03-15 17:10:31 +0100

    more

--- Jcpdf.java
+++ Jcpdf.java
@@ -1,11 +1,22 @@
 /* Jcpdf.java */
 
 public class Jcpdf {
-    native void helloFromC(); /* (1) */
+    native void startup();
+    native String version();
     static public void main(String argv[]) {
         System.loadLibrary("cpdf");
-        System.loadLibrary("jcpdf"); /* (2) */
+        System.loadLibrary("jcpdf");
         Jcpdf jcpdf = new Jcpdf();
-        jcpdf.helloFromC(); /* (3) */
+        /* CHAPTER 0. Preliminaries */
+        System.out.println("***** CHAPTER 0. Preliminaries");
+        System.out.println("---cpdf_startup()");
+        jcpdf.startup();
+        System.out.println("---cpdf_version()");
+        System.out.format("version = %s\n", jcpdf.version());
+        //Console.WriteLine("---cpdf_setFast()");
+        //Cpdf.setFast();
+        //Console.WriteLine("---cpdf_setSlow()");
+        //Cpdf.setSlow();
+        //Console.WriteLine("---cpdf_clearError()");
     }
 }
--- jcpdfwrapper.c
+++ jcpdfwrapper.c
@@ -4,10 +4,17 @@
 #include <stdio.h>
 #include "cpdflibwrapper.h"
 
-JNIEXPORT void JNICALL Java_Jcpdf_helloFromC
+JNIEXPORT void JNICALL Java_Jcpdf_startup
   (JNIEnv * env, jobject jobj)
 {
     char *argv[] = { "jcpdf", NULL };
     cpdf_startup(argv);
-    printf("Hello from C!\n");
+}
+
+JNIEXPORT jstring JNICALL Java_Jcpdf_version
+  (JNIEnv * env, jobject jobj)
+{
+    jstring result;
+    result = (*env)->NewStringUTF(env, cpdf_version());
+    return result;
 }
