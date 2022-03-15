31074dd8731064161464a0804550fb86c009994c
John Whitington <john@coherentgraphics.co.uk>
2022-03-15 16:41:30 +0100

    First use of cpdf module from java

--- Jcpdf.java
+++ Jcpdf.java
@@ -3,6 +3,7 @@
 public class Jcpdf {
     native void helloFromC(); /* (1) */
     static public void main(String argv[]) {
+        System.loadLibrary("cpdf");
         System.loadLibrary("jcpdf"); /* (2) */
         Jcpdf jcpdf = new Jcpdf();
         jcpdf.helloFromC(); /* (3) */
--- jcpdfwrapper.c
+++ jcpdfwrapper.c
@@ -2,9 +2,12 @@
 
 #include <jni.h>
 #include <stdio.h>
+#include "cpdflibwrapper.h"
 
 JNIEXPORT void JNICALL Java_Jcpdf_helloFromC
   (JNIEnv * env, jobject jobj)
 {
+    char *argv[] = { "jcpdf", NULL };
+    cpdf_startup(argv);
     printf("Hello from C!\n");
 }
